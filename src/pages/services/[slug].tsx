5afcf7e62a0d6d2a421274b49a5efefaef69629c
nicholidev <nicholijin@gmail.com>
2023-02-11 02:19:58 +0800

    update

---
+++ src/pages/services/[slug].tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NextPage } from "next";
+import LandingLayout from "@layouts/landing";
+
+const ServicePage: NextPage = () => {
+    return (
+        <LandingLayout>
+
+        </LandingLayout>
+    );
+};
+
+export default ServicePage;
